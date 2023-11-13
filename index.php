6e744667e300ae8a61c54c2ac86b6f067cedc6d5
AlexanderLivanov <help.underlinegroup@gmail.com>
2023-11-13 21:10:49 +0300

    макет главной страницы

--- index.php
+++ index.php
@@ -6,12 +6,61 @@ require_once('system/configs/dbcfg.php');
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="system/static/content/css/style.css">
     <title>FotosEngine</title>
 </head>
+
 <body>
-    Здесь пока ничего нет...
+    <div class="container">
+        <div class="ubox">
+            <div class="ubox-header">
+
+            </div>
+            <div class="ubox-content">
+
+            </div>
+            <div class="ubox-footer">
+
+            </div>
+        </div>
+        <div class="ubox">
+            <div class="ubox-header">
+
+            </div>
+            <div class="ubox-content">
+
+            </div>
+            <div class="ubox-footer">
+
+            </div>
+        </div>
+        <div class="ubox">
+            <div class="ubox-header">
+
+            </div>
+            <div class="ubox-content">
+
+            </div>
+            <div class="ubox-footer">
+
+            </div>
+        </div>
+        <div class="ubox">
+            <div class="ubox-header">
+
+            </div>
+            <div class="ubox-content">
+
+            </div>
+            <div class="ubox-footer">
+
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
