68560084d2be7c960878bc87406e8c798d0b6117
AlexanderLivanov <help.underlinegroup@gmail.com>
2023-12-24 17:45:33 +0300

    дизайн ленты

--- index.php
+++ index.php
@@ -17,27 +17,55 @@ require_once('system/configs/dbcfg.php');
 <body>
     <div class="feed">
         <div class="feed-item">
-            <div class="card">
-                <img src="system/static/content/img/test.png" alt="">
+            <div class="img-container">
+                <div class="meta-box-head">
+                    SOME TEXT HERE
+                </div>
+                <div class="card" style="background: url('https://optgoods.ru/uploads/-ekrana-2023-07-13-v-14.19.01.png?1689247201559'); background: repeat scroll 0% 0% / auto padding-box border-box none transparent;">
+                    <img src="https://optgoods.ru/uploads/-ekrana-2023-07-13-v-14.19.01.png?1689247201559" alt="">
+                </div>
+                <div class="meta-box-foot">
+                    <div class="plus-btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M12 5l0 14" />
+                            <path d="M5 12l14 0" />
+                        </svg>
+                        <p>1234</p>
+                    </div>
+                    <div class="minus-btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-minus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M5 12l14 0" />
+                        </svg>
+                        <p>24</p>
+                    </div>
+                </div>
             </div>
-            <div class="card">
-                <img src="system/static/content/img/test.png" alt="">
-            </div>
-        </div>
-        <div class="feed-item">
-            <div class="card">
-                <img src="system/static/content/img/test.png" alt="">
-            </div>
-            <div class="card">
-                <img src="system/static/content/img/test.png" alt="">
-            </div>
-        </div>
-        <div class="feed-item">
-            <div class="card">
-                <img src="system/static/content/img/test.png" alt="">
-            </div>
-            <div class="card">
-                <img src="system/static/content/img/test.png" alt="">
+            <div class="img-container">
+                <div class="meta-box-head">
+                    SOME TEXT HERE
+                </div>
+                <div class="card">
+                    <img src="https://klike.net/uploads/posts/2023-01/1674913868_3-70.jpg" alt="">
+                </div>
+                <div class="meta-box-foot">
+                    <div class="plus-btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M12 5l0 14" />
+                            <path d="M5 12l14 0" />
+                        </svg>
+                        <p>1234</p>
+                    </div>
+                    <div class="minus-btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-minus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M5 12l14 0" />
+                        </svg>
+                        <p>24</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
