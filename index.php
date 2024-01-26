4962fddb503ee0c6ffc0e0e916090aba284af772
AlexanderLivanov <help.underlinegroup@gmail.com>
2024-01-26 22:29:36 +0300

    работа к контроллерами

--- index.php
+++ index.php
@@ -1,132 +1,20 @@
-<!-- 10.11.2023 (c) Alexander Livanov -->
-
-<?php
-require_once('system/configs/dbcfg.php');
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="system/static/content/css/style.css">
-    <title>FotosEngine</title>
+    <title>Fotos.World</title>
 </head>
 
 <body>
-    <header>
-        <?php require_once('system/static/header.php'); ?>
-    </header>
-    <div class="feed">
-        <div class="feed-item">
-            <div class="img-container">
-                <div class="meta-box-head">
-                    <p>David Frankel - <i>16M</i></p>
-                </div>
-                <div class="card">
-                    <img src="https://w.forfun.com/fetch/b9/b9eee85dfc19d16354e099213a89d4eb.jpeg" alt="">
-                </div>
-                <div class="meta-box-foot">
-                    <div class="plus-btn">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M12 5l0 14" />
-                            <path d="M5 12l14 0" />
-                        </svg>
-                        <p>1234</p>
-                    </div>
-                    <div class="minus-btn">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-minus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M5 12l14 0" />
-                        </svg>
-                        <p>24</p>
-                    </div>
-                </div>
-            </div>
-            <div class="img-container">
-                <div class="meta-box-head">
-                    <p>James Mangold - <i>23M</i></p>
-                </div>
-                <div class="card">
-                    <img src="https://images.freekaamaal.com/post_images/1650018178.webp" alt="">
-                </div>
-                <div class="meta-box-foot">
-                    <div class="plus-btn">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M12 5l0 14" />
-                            <path d="M5 12l14 0" />
-                        </svg>
-                        <p>1234</p>
-                    </div>
-                    <div class="minus-btn">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-minus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M5 12l14 0" />
-                        </svg>
-                        <p>24</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="feed-item">
-            <div class="img-container">
-                <div class="meta-box-head">
-                    <p>David Frankel - <i>16M</i></p>
-                </div>
-                <div class="card">
-                    <img src="https://w.forfun.com/fetch/b9/b9eee85dfc19d16354e099213a89d4eb.jpeg" alt="">
-                </div>
-                <div class="meta-box-foot">
-                    <div class="plus-btn">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M12 5l0 14" />
-                            <path d="M5 12l14 0" />
-                        </svg>
-                        <p>1234</p>
-                    </div>
-                    <div class="minus-btn">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-minus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M5 12l14 0" />
-                        </svg>
-                        <p>24</p>
-                    </div>
-                </div>
-            </div>
-            <div class="img-container">
-                <div class="meta-box-head">
-                    <p>James Mangold - <i>23M</i></p>
-                </div>
-                <div class="card">
-                    <img src="https://images.freekaamaal.com/post_images/1650018178.webp" alt="">
-                </div>
-                <div class="meta-box-foot">
-                    <div class="plus-btn">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M12 5l0 14" />
-                            <path d="M5 12l14 0" />
-                        </svg>
-                        <p>1234</p>
-                    </div>
-                    <div class="minus-btn">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-minus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M5 12l14 0" />
-                        </svg>
-                        <p>24</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <footer>
-        <?php require_once('system/static/footer.php'); ?>
-    </footer>
+    <?php require_once('system/static/header.php');
+        require_once('system/static/scripts/models/auth_controller.php');
+
+        echo("<br><br>");
+        $curr_user = new User();
+        print_r($curr_user);
+    ?>    
 </body>
 
 </html>
---
+++ system/static/scripts/models/auth_controller.php
@@ -0,0 +1,7 @@
+<?php 
+
+class User {
+    public $id, $username;
+}
+
+?>
