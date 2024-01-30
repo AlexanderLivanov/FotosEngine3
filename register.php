8a617ef9f57174b3fc3923779e46c391662de56b
Leonid Orlov <dot.slash.prompt@gmail.com>
2024-01-30 21:58:57 +0300

    server bug fixes

--- register.php
+++ register.php
@@ -17,20 +17,19 @@ if(!empty($_POST['register'])){
     $query = $connect->prepare("SELECT * FROM users WHERE username=:username");
     $query->bindParam("username", $username, PDO::PARAM_STR);
     $query->execute();
-
     if($query->rowCount() > 0){
         echo('<p>Это имя пользователя уже занято</p>');
     }
     if($query->rowCount() == 0){
-        $query = $connect->prepare("INSERT INTO users(username, passwd, reg_date, last_activity, invited_by) VALUES 
-                                                    (:username, :passwd_hash, :local_datetime, :local_datetime, :ref)");
+        $query = $connect->prepare("INSERT INTO users(username, passwd, reg_date, last_activity, invited_by, rating, role, ip) VALUES 
+                                                    (:username, :passwd_hash, :local_datetime, :local_datetime, :ref, '1', '0', '0:0:0:0');");
         $query->bindParam("username", $username, PDO::PARAM_STR);
         $query->bindParam("passwd_hash", $passwd_hash, PDO::PARAM_STR);
         $query->bindParam("local_datetime", $local_datetime, PDO::PARAM_STR);
         $query->bindParam("local_datetime", $local_datetime, PDO::PARAM_STR);
         $query->bindParam("ref", $ref, PDO::PARAM_STR);
-
         $result = $query->execute();
+
         if($result){
             echo("<p>Регистрация прошла успешно.<br> Сейчас вы будете перенаправлены на страницу входа</p>");
             echo("<script>setTimeout(function () { window.location.href = 'login.php'; }, 1000);</script>");
@@ -54,13 +53,12 @@ if(!empty($_POST['register'])){
 <body>
     <form method="post" class="auth_form">
         <h1>FotosWorld - Создание учетной записи</h1>
-        <input type="text" placeholder="придумайте имя пользователя" name="username" pattern="[A-Za-z._-]{4,}" required>
+        <input type="text" placeholder="придумайте имя пользователя" name="username" pattern="[A-Za-z._-]{5,}" required>
         <input type="password" placeholder="придумайте пароль" name="passwd" minlength="8" required>
-        <input type="text" placeholder="кто пригласил?" name="ref" pattern="[A-Za-z]{4,}">
+        <input type="text" placeholder="кто пригласил?" name="ref" pattern="[A-Za-z._-]{5,}">
         <input type="submit" name="register" value="Готово">
     </form>
 </body>
 
 </html>
 
-<!-- Юле: спасибо) -->
