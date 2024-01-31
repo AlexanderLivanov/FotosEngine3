e1f44afbc10921aa4b8e876636e31d1c1a8e53a6
AlexanderLivanov <help.underlinegroup@gmail.com>
2024-01-31 22:14:05 +0300

    login page styles

--- login.php
+++ login.php
@@ -30,7 +30,7 @@ if (isset($_POST['login'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/pages.css">
+    <link rel="stylesheet" href="system/static/content/css/style.css">
     <title>Вход — FotosCMS</title>
 </head>
 
