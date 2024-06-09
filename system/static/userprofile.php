b9e699de09b76f5420a5d659904b090b1e7f0889
AlexanderLivanov <help.underlinegroup@gmail.com>
2024-06-09 12:47:43 +0300

    подключение tinyMCE

--- system/static/userprofile.php
+++ system/static/userprofile.php
@@ -2,16 +2,32 @@
 // 13.03.2024 (c) Alexander Livanov
 
 if (!empty($_POST['session_destroy'])) {
-    echo('destr sess');
+    echo ('destr sess');
     session_destroy();
     setcookie("FW_AUTH_TOKEN", "", strtotime('+30 days'));
-    echo('<script>window.location.replace("/");</script>');
+    echo ('<script>window.location.replace("/");</script>');
 }
 ?>
 <html>
 
 <head>
     <link rel="stylesheet" href="css/userprofile.css">
+    <style>
+        #container {
+            margin: 20px auto;
+        }
+
+        .ck-editor__editable[role="textbox"] {
+            /* Editing area */
+            min-height: 200px;
+        }
+
+        .ck-content .image {
+            /* Block images */
+            max-width: 80%;
+            margin: 20px auto;
+        }
+    </style>
 </head>
 
 <body>
@@ -36,18 +52,28 @@ if (!empty($_POST['session_destroy'])) {
         <div id="gallery" class="tab-content">
             <h3>Галерея</h3>
             <?php
-                require_once('system/static/gallery.php');
+            require_once('system/static/gallery.php');
             ?>
         </div>
         <div id="posts" class="tab-content" style="display: none;">
             <h3>Посты</h3>
+            <div id="container">
+                <textarea name="post_title" id="" placeholder="Название поста" style="width: 90%; border: none; border-bottom: 2px solid #0072ff; outline: none;"></textarea>
+                <textarea id="default" placeholder="Краткость - сестра таланта..."></textarea>
+                <input type="submit" value="Создать пост" style="border-radius: 15px; color: #0072ff; padding: 1em;" onclick="alert(tinymce.activeEditor.getContent());">
+            </div>
         </div>
         <div id="info" class="tab-content" style="display: none;">
             <h3>Настройки аккаунта</h3>
-            <?php echo("<form method='post'><button type='submit' value='session_destroy' name='session_destroy'>Выйти</button></form>"); ?>
+            <?php echo ("<form method='post'><button type='submit' value='session_destroy' name='session_destroy'>Выйти</button></form>"); ?>
         </div>
     </div>
-
+    <script src="https://cdn.tiny.cloud/1/qz8i2t9v3yqmvp0hyjlv95kybrn89u3py39nj1efjraq0e9p/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
+    <script>
+        tinymce.init({
+            selector: 'textarea#default'
+        });
+    </script>
     <script>
         function openTab(tabName) {
             var i, tabcontent, tablinks;
